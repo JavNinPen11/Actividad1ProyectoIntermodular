54dc5f0d37e62d010c8199cbfbb0f735282bdde8
PAULA ESCRIBANO REINA <pauescrei@alu.edu.gva.es>
2025-10-02 13:21:40 +0200

    Creacion del metodo persona mayor o menor

--- Actividad1ProyectoIntermodular/Actividad1ProyectoIntermodular/Program.cs
+++ Actividad1ProyectoIntermodular/Actividad1ProyectoIntermodular/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Actividad1ProyectoIntermodular;
+
+Persona persona1 = new Persona("Paula", 20);
+
+Console.WriteLine(persona1.MostrarDatos());
+
+Console.WriteLine(persona1.EsMayorDeEdad());
+
+Console.ReadLine();
